14dae65a87ecf7ef4ac703e9d7699bfc6b6fa5ac
Ayush Munjal <AMunjal@sunwing.ca>
2019-04-21 10:48:54 -0400

    spicMacay1 Fix

---
+++ spicMacay1/precache-manifest.6a69811de03241243957e9213ae2c6c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spicmacay1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d564ecc8da899af19aff90f9bbf8ecaa",
+    "url": "/spicmacay1/static/media/blog-post.3.d564ecc8.md"
+  },
+  {
+    "revision": "8c6ad8d34fb944fedb72c536f63ea434",
+    "url": "/spicmacay1/static/media/blog-post.2.8c6ad8d3.md"
+  },
+  {
+    "revision": "9dbfaeb9bd1beabd55a57a27e003dd5a",
+    "url": "/spicmacay1/static/media/blog-post.1.9dbfaeb9.md"
+  },
+  {
+    "revision": "a06c4b2fdd598623903a",
+    "url": "/spicmacay1/static/js/runtime~main.eae8959a.js"
+  },
+  {
+    "revision": "23f11a2a45856820e789",
+    "url": "/spicmacay1/static/js/main.21b444e3.chunk.js"
+  },
+  {
+    "revision": "57080765ea2330e6d1a2",
+    "url": "/spicmacay1/static/js/2.608a0082.chunk.js"
+  },
+  {
+    "revision": "23f11a2a45856820e789",
+    "url": "/spicmacay1/static/css/main.89ae3418.chunk.css"
+  },
+  {
+    "revision": "83f4364458555bc84581db557e49a870",
+    "url": "/spicmacay1/index.html"
+  }
+];
--- spicMacay1/service-worker.js
+++ spicMacay1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/spicmacay/precache-manifest.e8c316b876ba16fbe67513eba871ab48.js"
+  "/spicmacay1/precache-manifest.6a69811de03241243957e9213ae2c6c6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/spicmacay/index.html", {
+workbox.routing.registerNavigationRoute("/spicmacay1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
