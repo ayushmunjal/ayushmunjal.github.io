43147178983d81a993438ab53f6a59546b486224
Ayush Munjal <AMunjal@sunwing.ca>
2019-04-21 10:06:48 -0400

    spic macay

---
+++ spicmacay/precache-manifest.f4264dbe98da67cac321fca90ff92007.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spicmacay/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d564ecc8da899af19aff90f9bbf8ecaa",
+    "url": "/spicmacay/static/media/blog-post.3.d564ecc8.md"
+  },
+  {
+    "revision": "8c6ad8d34fb944fedb72c536f63ea434",
+    "url": "/spicmacay/static/media/blog-post.2.8c6ad8d3.md"
+  },
+  {
+    "revision": "9dbfaeb9bd1beabd55a57a27e003dd5a",
+    "url": "/spicmacay/static/media/blog-post.1.9dbfaeb9.md"
+  },
+  {
+    "revision": "259aa16fba4f0a2eaae5",
+    "url": "/spicmacay/static/js/runtime~main.38ec936c.js"
+  },
+  {
+    "revision": "9f05b255e0f00d016b05",
+    "url": "/spicmacay/static/js/main.36452060.chunk.js"
+  },
+  {
+    "revision": "57080765ea2330e6d1a2",
+    "url": "/spicmacay/static/js/2.608a0082.chunk.js"
+  },
+  {
+    "revision": "9f05b255e0f00d016b05",
+    "url": "/spicmacay/static/css/main.89ae3418.chunk.css"
+  },
+  {
+    "revision": "6119c61ddf05181f7822f531054fe414",
+    "url": "/spicmacay/index.html"
+  }
+];
